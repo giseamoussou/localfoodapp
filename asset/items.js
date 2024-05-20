157dbda1e3c0c3ab4eca6b544c2c3ea98ccf5c37
giseamoussou <amoussougise@gmail.com>
2024-05-20 13:53:21 +0200

    Nouveau depot

---
+++ asset/items.js
@@ -0,0 +1,46 @@
+import { Image } from "react-native"
+
+export const COLORS ={
+    white:'#ffffff',
+    black:'#000000',
+    ligthGray:'#838486',
+    accent:'#FFC231',
+    accentRed:'#FB5D2E',
+    accentPink:'F96165'
+
+}
+export const Categories ={
+    name:'Burger',
+    image: require('../database/images/ice_cream.png'),
+    items:[
+        {
+            name:'Classic Burguer',
+            weight:150,
+            rating:'4.5',
+            price: 99,
+            isTopOfTheWeek:false,
+            image:require('../database/images/burger/burger2.jpg'),
+            size : 'Large 12"',
+            crust: 'Thick Crust',
+            delivery:25,
+            ingredients:{
+                require
+            }
+    },
+    {
+        name:'Pizza',
+        weight:150,
+        rating:'4.2',
+        price:99,
+        isTopOfTheWeek:false,
+        image:require('../database/images/pizza/pizza1.jpg'),
+        size : 'Large Glass"',
+        crust: 'Small ice',
+        delivery: 5,
+        ingredients:{
+            require
+        }
+}
+]
+    
+}
---
+++ components/Details.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const  Details = () => {
+  return (
+    <View>
+      <Text>This is details</Text>
+    </View>
+  )
+}
+export default Details
