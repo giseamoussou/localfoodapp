6833a688f35863e006c5e021b60f281d76f56d5d
ergen35 <ienoverse@gmail.com>
2024-05-25 08:03:31 +0100

    exporting supabase client from services

--- services/supabase-client.ts
+++ services/supabase-client.ts
@@ -1,10 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
 import Config from 'react-native-config';
 
-<<<<<<< HEAD
-const supabase = createClient(Config.SUPABASE_URL, Config.SUPABASE_KEY)
-
-export default supabase;
-=======
 export const supabase = createClient(Config.SUPABASE_URL, Config.SUPABASE_KEY)
->>>>>>> 9d4ce6b4d74709fa02744b5a4d77fee85762b52c
