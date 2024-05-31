547aa2a009020a2ce71e3dc3b16c856e14980e36
ergen35 <ienoverse@gmail.com>
2024-05-31 12:40:43 +0100

    Updated Admin dashboard

---
+++ admin-app/src/lib/components/DashboardMenuItem/DashboardMenuItem.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import Icon from "@iconify/svelte";
+
+    export let title: string
+    export let iconName: string;
+</script>
+
+<div class="w-full shadow-sm shadow-[tomato] bg-yellow-50 h-auto rounded-lg flex flex-col space-y-1 border p-2 border-[tomato] transition-all hover:scale-95 cursor-pointer text-[tomato] duration-300 hover:text-white hover:border-white hover:bg-[tomato]">
+    <Icon icon={iconName} class="w-8 h-8 mx-auto" />
+    <span class="block text-center text-xs">{title}</span>
+</div>
--- admin-app/src/routes/dashboard/+layout.svelte
+++ admin-app/src/routes/dashboard/+layout.svelte
@@ -1,26 +1,36 @@
 <script lang="ts">
     import Icon from '@iconify/svelte';
 import type { LayoutData } from './$types';
+    import DashboardMenuItem from '$lib/components/DashboardMenuItem/DashboardMenuItem.svelte';
     
     export let data: LayoutData;
 </script>
 
 
-<div class="w-full h-full flex flex-row bg-fuchsia-800">
+<div class="w-full h-full flex flex-row ">
 
-    <div class="h-full w-40">
-        <div class="w-full h-full py-6 px-3">
-            <div class="shadow-sm border-gray-300 bg-yellow-50 h-full w-full">
-                <div class="w-full h-auto rounded-lg flex flex-col space-y-2 bo">
-                    <Icon icon="material-symbols:dashboard-customize-rounded" color="tomato" class="w-8 h-8 mx-auto" />
-                    <span class="block text-center">Dashboard</span>
-                </div>
+    <div class="h-full w-36">
+        <div class="w-full h-full py-6 px-2">
+            <div class="shadow-sm rounded-lg border-gray-300 bg-yellow-50 h-full w-full p-3 space-y-2">
+                
+                <DashboardMenuItem iconName="material-symbols:dashboard-customize-rounded" title="Dashboard" />
+                
+                <DashboardMenuItem iconName="ion:fast-food-sharp" title="Plats" />
+                
+                <DashboardMenuItem iconName="arcticons:foodora" title="Commandes" />
+                
+                <DashboardMenuItem iconName="fa-solid:users-cog" title="Utilisateurs" />
+                
+                <DashboardMenuItem iconName="material-symbols:admin-panel-settings-rounded" title="Admins" />
+            
             </div>
         </div>
     </div>
     
-    <main class="flex flex-1 h-full">
-        <slot />
+    <main class="flex flex-1 h-full p-5">
+        <div class="flex h-full w-full border border-slate-300 content-center items-center justify-center shadow-sm">
+            <slot />
+        </div>
     </main>
 
 </div>
