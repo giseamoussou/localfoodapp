73256a46376f664c57895326d91b5e1437671297
ergen35 <ienoverse@gmail.com>
2024-06-07 11:18:19 +0100

    Minor Update

--- localfoodapp/components/CartItemDisplay.tsx
+++ localfoodapp/components/CartItemDisplay.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { Image, Text, View, TouchableOpacity, StyleSheet } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
 import MaComIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 
 export const CartItemDisplay = ({ item, removeFromCart }: { item: { id: any, name: string, price: number, quantity: number }, removeFromCart: (id: string) => void }) => {
-
-    const { name, price, quantity } = item;
-    const totalPrice = price * quantity;
-
+    
     return (
 
         <TouchableOpacity activeOpacity={0.80} style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 15, backgroundColor: '#fff', paddingHorizontal: 10, paddingVertical: 10, elevation: 1, borderRadius: 15, margin: 2 }}>
@@ -17,7 +14,7 @@ export const CartItemDisplay = ({ item, removeFromCart }: { item: { id: any, nam
                 <Text style={{ fontSize: 14, color: '#888' }}>Qte: {item.quantity}</Text>
             </View>
             <View style={{ flexDirection: 'row', alignItems: 'center', marginHorizontal: 0 }}>
-                <Text style={{ fontSize: 16, marginHorizontal: 0, color: '#333' }}>Total: <Text style={{ color: 'tomato' }}>{totalPrice}</Text></Text>
+                <Text style={{ fontSize: 16, marginHorizontal: 0, color: '#333' }}>Total: <Text style={{ color: 'tomato' }}>{item.price * item.quantity}</Text></Text>
             </View>
             <TouchableOpacity activeOpacity={0.70} style={{ marginStart: 15 }} onPress={() => removeFromCart(item.id)}>
                 <MaComIcons name="cart-remove" size={30} color='crimson' />
