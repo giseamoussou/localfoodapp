e0d4be116fa72923df33f355671b98fbb9525ad6
ergen35 <ienoverse@gmail.com>
2024-06-07 01:09:54 +0100

    Added payment

--- localfoodapp/screens/ShoppingCartScreen.tsx
+++ localfoodapp/screens/ShoppingCartScreen.tsx
@@ -1,8 +1,9 @@
-import { View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Text, Button, StyleSheet, TouchableOpacity, ToastAndroid, Linking, Alert } from 'react-native'
 import React, { useContext, useEffect, useState } from 'react'
 import { ShoppingCartContext } from '../contexts/Context'
 import { ScrollView } from 'react-native';
 import EvilIcons from 'react-native-vector-icons/EvilIcons';
+import InAppBrowser from 'react-native-inappbrowser-reborn';
 
 function ShoppingCartScreen() {
     const { cartContext, setCartContext } = useContext(ShoppingCartContext)
@@ -14,63 +15,107 @@ function ShoppingCartScreen() {
 
     }, [cartContext, cartContext.cart])
 
-    function orderCommand() {
+    async function orderCommand() {
 
+        const paymentLink = new URL("https://localfoodapp.onrender.com/pay").toString();
+
+        ToastAndroid.show("Redirection vers la page de paiement", ToastAndroid.LONG);
+
+
+        //Open In Browser
+        try {
+            const isAvailable = await InAppBrowser.isAvailable()
+            const url = paymentLink
+            if (isAvailable) {
+                InAppBrowser.open(url, {
+                    // iOS Properties
+                    dismissButtonStyle: 'cancel',
+                    preferredBarTintColor: 'gray',
+                    preferredControlTintColor: 'white',
+                    // Android Properties
+                    showTitle: true,
+                    showInRecents: false,
+                    toolbarColor: 'indigo',
+                    secondaryToolbarColor: 'black',
+                    enableUrlBarHiding: true,
+                    enableDefaultShare: false,
+                    forceCloseOnRedirection: true,
+                })
+                    .then((result) => {
+
+                        if (result.type == "cancel") {
+                            ToastAndroid.show("Opération Annulée", ToastAndroid.SHORT);
+                        }
+                    });
+            }
+            else {
+                try {
+                    Linking.openURL(url);
+                }
+                catch (error) {
+                    Alert.alert("Impossible d'ouvrir la page de paiement...");
+                }
+            }
+        } 
+        catch (error) {
+        
+        }
+        //End
     }
 
-    function removeFromCart(id: any) {
+function removeFromCart(id: any) {
 
-        const updatedCart = cartContext.cart.filter(item => item.id !== id);
-        if (updatedCart.length < cartContext.cart.length) {
-            const existingProduct = cartContext.cart.find(item => item.id === id);
-            if (existingProduct) {
-                if (existingProduct?.quantity > 1) {
-                    updatedCart.push({ ...existingProduct, quantity: existingProduct.quantity - 1 });
-                }
-                setCartContext({ ...cartContext, cart: updatedCart });
+    const updatedCart = cartContext.cart.filter(item => item.id !== id);
+    if (updatedCart.length < cartContext.cart.length) {
+        const existingProduct = cartContext.cart.find(item => item.id === id);
+        if (existingProduct) {
+            if (existingProduct?.quantity > 1) {
+                updatedCart.push({ ...existingProduct, quantity: existingProduct.quantity - 1 });
             }
+            setCartContext({ ...cartContext, cart: updatedCart });
         }
     }
+}
 
-    return (
-        <>
-            <View style={styles.container}>
-                {/* <View style={styles.header}>
+return (
+    <>
+        <View style={styles.container}>
+            {/* <View style={styles.header}>
                     <Text style={styles.headerText}>Mon Panier</Text>
                 </View> */}
 
-                <ScrollView style={styles.cartList}>
-                    {cartContext.cart && cartContext.cart.length > 0 && cartContext.cart.map((item) => (
-                        <View key={item.id} style={styles.cartItem}>
-                            <Text style={styles.cartItemText}>{item.name}</Text>
-                            <Text style={styles.cartItemText}>Qte: {item.quantity}</Text>
-                            <Text style={styles.cartItemText}>Prix: {item.price}</Text>
-                            <Text style={styles.cartItemText}>Total: {item.price * item.quantity}</Text>
-                            <Button title="Supprimer" onPress={() => removeFromCart(item.id)} />
-                        </View>
-                    ))}
-                    {
-                        cartContext.cart && cartContext.cart.length <= 0 &&
-                        <View style={{ alignItems: 'center', alignContent: 'center', justifyContent: 'center', flex: 1, display: 'flex', flexDirection: 'column', marginTop: 150 }}>
-                            <EvilIcons name='cart' size={90} color='darkslategray' />
-                            <Text style={{ color: 'black', fontSize: 18 }}>Panier Vide</Text>
-                        </View>
-                    }
-                </ScrollView>
-
+            <ScrollView style={styles.cartList}>
+                {cartContext.cart && cartContext.cart.length > 0 && cartContext.cart.map((item) => (
+                    <View key={item.id} style={styles.cartItem}>
+                        <Text style={styles.cartItemText}>{item.name}</Text>
+                        <Text style={styles.cartItemText}>Qte: {item.quantity}</Text>
+                        <Text style={styles.cartItemText}>Prix: {item.price}</Text>
+                        <Text style={styles.cartItemText}>Total: {item.price * item.quantity}</Text>
+                        <Button title="Supprimer" onPress={() => removeFromCart(item.id)} />
+                    </View>
+                ))}
                 {
-                    (total > 0) &&
-                    <View style={styles.totalContainer}>
-                        <Text style={styles.totalText}>Total: <Text style={{ color: 'tomato' }}>{cartContext.cart.reduce((total, item) => total + item.price * item.quantity, 0)}</Text></Text>
-                        <TouchableOpacity activeOpacity={0.75} style={{ marginHorizontal: 30, paddingVertical: 12, backgroundColor: 'black', borderRadius: 12, elevation: 5 }} onPress={orderCommand}>
-                            <Text style={{ textAlign: 'center', color: 'white' }}>Commander</Text>
-                        </TouchableOpacity>
+                    cartContext.cart && cartContext.cart.length <= 0 &&
+                    <View style={{ alignItems: 'center', alignContent: 'center', justifyContent: 'center', flex: 1, display: 'flex', flexDirection: 'column', marginTop: 150 }}>
+                        <EvilIcons name='cart' size={90} color='darkslategray' />
+                        <Text style={{ color: 'black', fontSize: 18 }}>Panier Vide</Text>
                     </View>
                 }
-            </View>
+            </ScrollView>
 
-        </>
-    )
+            {
+                (total > 0) &&
+                <View style={styles.totalContainer}>
+                    <Text style={styles.totalText}>Total: <Text style={{ color: 'tomato' }}>{cartContext.cart.reduce((total, item) => total + item.price * item.quantity, 0)}</Text></Text>
+                    <TouchableOpacity activeOpacity={0.75} style={{ marginHorizontal: 30, paddingVertical: 12, backgroundColor: 'black', borderRadius: 12, elevation: 5 }} onPress={orderCommand}>
+                        <Text style={{ textAlign: 'center', color: 'white' }}>Commander</Text>
+                    </TouchableOpacity>
+                </View>
+            }
+        </View>
+
+    </>
+)
 }
 
 const styles = StyleSheet.create({
