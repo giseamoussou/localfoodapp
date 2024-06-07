330db48b865f62521f6204ee80faab2f7b9f8155
ergen35 <ienoverse@gmail.com>
2024-06-07 16:58:17 +0100

    Added welcome page

---
+++ admin-app/src/routes/welcome/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+</script>
+
+
+<center class="flex flex-col justify-center content-center items-center h-1/2">
+    <div class="text-slate-800 text-2xl">
+        Bienvenue sur LocalFood App, votre compte a été été correctement activé.
+        <div class="text-[tomato]">
+            Bonne dégustation
+        </div> 
+    </div>
+</center>
